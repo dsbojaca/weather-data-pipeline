0642e0562b4c1c1ac472ebfcca1e2237cc3e0e8c
dsbojaca <dsbojacar@gmail.com>
2025-07-17 10:25:40 -0500

    Funcionamiento de la funcion con una lista de ciudades con una lectura desde un txt

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from src.extract import parse_weather_data
 from src.transform import parse_weather
 from src.load import save_to_db
 
-def run_pipeline(city):
 
-    print(f"obteniendo datos de {city}...")
-    raw= parse_weather_data(city)
-    parsed= parse_weather(raw)
-    save_to_db(parsed)
+def leer_ciudades(path="./env_clima/app/cities.txt"):
+    with open(path, "r", encoding="utf-8") as file:
+        return [line.strip() for line in file if line.strip()]
+    
+def run_pipeline(cities):
+    for city in cities:
+        print(f"obteniendo datos de {city}...")
+        raw= parse_weather_data(city)
+        parsed= parse_weather(raw)
+        save_to_db(parsed)
 
 
 if __name__== "__main__":
-    city = input("Ingrese la ciudad: ")
-    run_pipeline(city)
+    cities= leer_ciudades()
+    run_pipeline(cities)
